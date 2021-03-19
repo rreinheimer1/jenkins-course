56cc01373b58b5701e6ac3319d7b709ef3c38fca
rreinheimer1 <78128459+rreinheimer1@users.noreply.github.com>
2021-03-19 16:59:18 -0700

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('rreinheimer/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('rreinheimer')
+            registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
